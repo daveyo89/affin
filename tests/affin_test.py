309aa8ef7829da75474e9eb06c9c6bdb7b5d4179
daveyo89 <j.tothdavid@gmail.com>
2020-04-06 12:58:27 +0200

    jenk step test

--- tests/affin_test.py
+++ tests/affin_test.py
@@ -9,7 +9,15 @@ class AffinTest(unittest.TestCase):
         encoded = af.encode((97, 15), text)
         decoded = af.decode((97, 15), encoded)
         self.assertEqual(text, decoded, msg=f"{text} -> {encoded} -> {decoded}")
-        print("Test complete")
+
+    def test_long_encode(self):
+        af = core.Affin()
+        text = "A Babar 1989-től 2001-ig vetített kanadai–francia televíziós rajzfilmsorozat, amely Jean de Brunhoff 1931-ben megjelent könyvéből készült. Az animációs játékfilmsorozat rendezői Larry Jacobs, Raymond Jafelice, Laura Shepherd és Dale Schott. A zenéjét Milan Kymlicka szerezte. "
+        encoded = af.encode((97, 15), text)
+        decoded = af.decode((97, 15), encoded)
+        self.assertEqual(text, decoded, msg=f"{text} -> {encoded} -> {decoded}")
+        print(encoded)
+        print(decoded)
 
 
 if __name__ == '__main__':
